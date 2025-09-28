1c30434d416bd359cdd385086301a61d80270f72
MaverickPoly <realahmadjon82@gmail.com>
2025-09-29 00:27:33 +0500

    Projects

---
+++ C++/2093A.cpp
@@ -0,0 +1,16 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+    int t;
+    cin >> t;
+
+    while(t--) {
+        int k;
+        cin >> k;
+
+        cout << (k % 2 == 0 ? "No" : "Yes") << endl;
+    }
+
+    return 0;
+}
